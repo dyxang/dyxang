e3bfdf915a14b1e325e418573f3eed6b3a84cd13
Dyxang <e1536155154@qq.com>
2020-12-19 19:12:14 +0800

    new commit

--- push.bat
+++ push.bat
@@ -1,2 +1,2 @@
 @echo off
-cd E:/Github/blogpush&&git add .&&git commit -m "new commit"&&git push -u origin main
+cd E:/Github/blogpush|git add .&&git commit -m "new commit"&&git push -u origin main
